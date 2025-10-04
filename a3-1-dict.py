c6caafc0b10ef361fc934b868768001b7a8ffa39
albertoquiroga-ctrl <albertoquiroga@enlacenest.com>
2025-10-04 10:28:35 +0800

    adding new class files

---
+++ a3-1-dict.py
@@ -0,0 +1,19 @@
+vocabs = {}
+
+
+type(vocabs)
+
+vocabs['programming'] = 'the activity of job of writing computer programs'
+vocabs
+
+
+vocabs['python'] = 'a very large snake that kills animals for food'
+
+del(vocabs['programming'])
+
+vocabs.clear()
+
+vocabs['programming'] = ['the activity of job of writing computer programs','_______ming']
+
+vocabs = {'programming': 'the activity of job of writing computer programs',
+          'python': 'a very large snake'}
